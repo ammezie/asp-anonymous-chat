bfa557ab87be02299aa2e1c2c0a71ba59ec8f9e2
samuel <samuel@WIN-CB74IDCL82O>
2017-03-17 19:02:08 +0000

    Add project files.

---
+++ pusherasp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pusherasp.Startup))]
+namespace pusherasp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
